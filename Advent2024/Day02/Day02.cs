55b92de09df08fba83796977e75c027ccfdb0eaf
Steve <125539+steve7411@users.noreply.github.com>
2024-12-04 14:18:38 -0500

    Renaming some variables and tiny refactor of day 2

--- Advent2024/Day02/Day02.cs
+++ Advent2024/Day02/Day02.cs
@@ -16,8 +16,8 @@ internal class Day02 : DayBase {
             if (validNoChange == 1)
                 continue;
             
-            var (testIdx, delta) = failIdx == 1 ? (failIdx + 1, -1) : (failIdx, -2);
-            var (isSafe1Remove, failIdx2) = IsValid(span, testIdx, span[testIdx + delta], expectedSign);
+            var (nextIdx, prevIdx) = failIdx == 1 ? (failIdx + 1, failIdx) : (failIdx, failIdx - 2);
+            var (isSafe1Remove, failIdx2) = IsValid(span, nextIdx, span[prevIdx], expectedSign);
             if (failIdx + 1 >= failIdx2)
                 (isSafe1Remove, _) = IsValid(span, failIdx + 1, span[failIdx - 1], expectedSign);
             safeCount1Removal += isSafe1Remove;
